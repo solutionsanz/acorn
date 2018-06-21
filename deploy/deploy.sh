18926465bb2625e5fcd7af81ecdfea543984ce41
SolutionsANZ <solutionsanz2@gmail.com>
2018-06-21 15:19:09 +1000

    fixing error in folder name

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,6 +3,6 @@
   #acorn..
   #git clone --quiet https://github.com/solutionsanz/acorn >>/tmp/noise.out && cd acorn
   kubectl create namespace acorn >>/tmp/noise.out
-  kubectl create -f kubernetes/kubernetes/acorn-dpl.yaml >>/tmp/noise.out
-  kubectl create -f kubernetes/kubernetes/acorn-svc.yaml >>/tmp/noise.out
-  kubectl create -f kubernetes/kubernetes/acorn-ing.yaml >>/tmp/noise.out
+  kubectl create -f kubernetes/acorn-dpl.yaml >>/tmp/noise.out
+  kubectl create -f kubernetes/acorn-svc.yaml >>/tmp/noise.out
+  kubectl create -f kubernetes/acorn-ing.yaml >>/tmp/noise.out
